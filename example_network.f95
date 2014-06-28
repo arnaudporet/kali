4e6871f38cdf80626ffd19ca2bf94e5012f043e7
Arnaud Poret <arnaud.poret@gmail.com>
2014-06-28 19:01:47 +0200

    .

--- example_network.f95
+++ example_network.f95
@@ -55,27 +55,17 @@ program example_network
         real,dimension(:,:)::x
         integer::k
         real,dimension(size(x,1),1)::y
-        !CycD
-        y(1,1)=x(1,k)
-        !Rb
-        y(2,1)=max(min(1.0-x(1,k),1.0-x(4,k),1.0-x(5,k),1.0-x(10,k)),min(x(6,k),1.0-x(1,k),1.0-x(10,k)))
-        !E2F
-        y(3,1)=max(min(1.0-x(2,k),1.0-x(5,k),1.0-x(10,k)),min(x(6,k),1.0-x(2,k),1.0-x(10,k)))
-        !CycE
-        y(4,1)=min(x(3,k),1.0-x(2,k))
-        !CycA
+        y(1,1)=x(1,k)!CycD
+        y(2,1)=max(min(1.0-x(1,k),1.0-x(4,k),1.0-x(5,k),1.0-x(10,k)),min(x(6,k),1.0-x(1,k),1.0-x(10,k)))!Rb
+        y(3,1)=max(min(1.0-x(2,k),1.0-x(5,k),1.0-x(10,k)),min(x(6,k),1.0-x(2,k),1.0-x(10,k)))!E2F
+        y(4,1)=min(x(3,k),1.0-x(2,k))!CycE
         y(5,1)=max(min(x(3,k),1.0-x(2,k),1.0-x(7,k),1.0-min(x(8,k),x(9,k))),&
-        min(x(5,k),1.0-x(2,k),1.0-x(7,k),1.0-min(x(8,k),x(9,k))))
-        !p27
-        y(6,1)=max(min(1.0-x(1,k),1.0-x(4,k),1.0-x(5,k),1.0-x(10,k)),min(x(6,k),1.0-min(x(4,k),x(5,k)),1.0-x(10,k),1.0-x(1,k)))
-        !Cdc20
-        y(7,1)=x(10,k)
-        !Cdh1
-        y(8,1)=max(min(1.0-x(5,k),1.0-x(10,k)),x(7,k),min(x(6,k),1.0-x(10,k)))
-        !UbcH10
-        y(9,1)=max(1.0-x(8,k),min(x(8,k),x(9,k),max(x(7,k),x(5,k),x(10,k))))
-        !CycB
-        y(10,1)=min(1.0-x(7,k),1.0-x(8,k))
+        min(x(5,k),1.0-x(2,k),1.0-x(7,k),1.0-min(x(8,k),x(9,k))))!CycA
+        y(6,1)=max(min(1.0-x(1,k),1.0-x(4,k),1.0-x(5,k),1.0-x(10,k)),min(x(6,k),1.0-min(x(4,k),x(5,k)),1.0-x(10,k),1.0-x(1,k)))!p27
+        y(7,1)=x(10,k)!Cdc20
+        y(8,1)=max(min(1.0-x(5,k),1.0-x(10,k)),x(7,k),min(x(6,k),1.0-x(10,k)))!Cdh1
+        y(9,1)=max(1.0-x(8,k),min(x(8,k),x(9,k),max(x(7,k),x(5,k),x(10,k))))!UbcH10
+        y(10,1)=min(1.0-x(7,k),1.0-x(8,k))!CycB
     end function f_physio
     !##########################################################################!
     !#############################    f_patho    ##############################!
@@ -88,27 +78,17 @@ program example_network
         real,dimension(:,:)::x
         integer::k
         real,dimension(size(x,1),1)::y
-        !CycD
-        y(1,1)=x(1,k)
-        !Rb
-        y(2,1)=0
-        !E2F
-        y(3,1)=max(min(1.0-x(2,k),1.0-x(5,k),1.0-x(10,k)),min(x(6,k),1.0-x(2,k),1.0-x(10,k)))
-        !CycE
-        y(4,1)=min(x(3,k),1.0-x(2,k))
-        !CycA
+        y(1,1)=x(1,k)!CycD
+        y(2,1)=0!Rb
+        y(3,1)=max(min(1.0-x(2,k),1.0-x(5,k),1.0-x(10,k)),min(x(6,k),1.0-x(2,k),1.0-x(10,k)))!E2F
+        y(4,1)=min(x(3,k),1.0-x(2,k))!CycE
         y(5,1)=max(min(x(3,k),1.0-x(2,k),1.0-x(7,k),1.0-min(x(8,k),x(9,k))),&
-        min(x(5,k),1.0-x(2,k),1.0-x(7,k),1.0-min(x(8,k),x(9,k))))
-        !p27
-        y(6,1)=max(min(1.0-x(1,k),1.0-x(4,k),1.0-x(5,k),1.0-x(10,k)),min(x(6,k),1.0-min(x(4,k),x(5,k)),1.0-x(10,k),1.0-x(1,k)))
-        !Cdc20
-        y(7,1)=x(10,k)
-        !Cdh1
-        y(8,1)=max(min(1.0-x(5,k),1.0-x(10,k)),x(7,k),min(x(6,k),1.0-x(10,k)))
-        !UbcH10
-        y(9,1)=max(1.0-x(8,k),min(x(8,k),x(9,k),max(x(7,k),x(5,k),x(10,k))))
-        !CycB
-        y(10,1)=min(1.0-x(7,k),1.0-x(8,k))
+        min(x(5,k),1.0-x(2,k),1.0-x(7,k),1.0-min(x(8,k),x(9,k))))!CycA
+        y(6,1)=max(min(1.0-x(1,k),1.0-x(4,k),1.0-x(5,k),1.0-x(10,k)),min(x(6,k),1.0-min(x(4,k),x(5,k)),1.0-x(10,k),1.0-x(1,k)))!p27
+        y(7,1)=x(10,k)!Cdc20
+        y(8,1)=max(min(1.0-x(5,k),1.0-x(10,k)),x(7,k),min(x(6,k),1.0-x(10,k)))!Cdh1
+        y(9,1)=max(1.0-x(8,k),min(x(8,k),x(9,k),max(x(7,k),x(5,k),x(10,k))))!UbcH10
+        y(10,1)=min(1.0-x(7,k),1.0-x(8,k))!CycB
     end function f_patho
 end program example_network
 
--- octave/example_network.m
+++ octave/example_network.m
@@ -36,26 +36,16 @@ function y=f_physio(x,k)
     #to cope with both boolean and multivalued logic, the Zadeh fuzzy logic
     #operators are used
     y=[
-    #CycD
-    x(1,k);
-    #Rb
-    max([min([1-x(1,k),1-x(4,k),1-x(5,k),1-x(10,k)]),min([x(6,k),1-x(1,k),1-x(10,k)])]);
-    #E2F
-    max([min([1-x(2,k),1-x(5,k),1-x(10,k)]),min([x(6,k),1-x(2,k),1-x(10,k)])]);
-    #CycE
-    min([x(3,k),1-x(2,k)]);
-    #CycA
-    max([min([x(3,k),1-x(2,k),1-x(7,k),1-min([x(8,k),x(9,k)])]),min([x(5,k),1-x(2,k),1-x(7,k),1-min([x(8,k),x(9,k)])])]);
-    #p27
-    max([min([1-x(1,k),1-x(4,k),1-x(5,k),1-x(10,k)]),min([x(6,k),1-min([x(4,k),x(5,k)]),1-x(10,k),1-x(1,k)])]);
-    #Cdc20
-    x(10,k);
-    #Cdh1
-    max([min([1-x(5,k),1-x(10,k)]),x(7,k),min([x(6,k),1-x(10,k)])]);
-    #UbcH10
-    max([1-x(8,k),min([x(8,k),x(9,k),max([x(7,k),x(5,k),x(10,k)])])]);
-    #CycB
-    min([1-x(7,k),1-x(8,k)])
+    x(1,k);#CycD
+    max([min([1-x(1,k),1-x(4,k),1-x(5,k),1-x(10,k)]),min([x(6,k),1-x(1,k),1-x(10,k)])]);#Rb
+    max([min([1-x(2,k),1-x(5,k),1-x(10,k)]),min([x(6,k),1-x(2,k),1-x(10,k)])]);#E2F
+    min([x(3,k),1-x(2,k)]);#CycE
+    max([min([x(3,k),1-x(2,k),1-x(7,k),1-min([x(8,k),x(9,k)])]),min([x(5,k),1-x(2,k),1-x(7,k),1-min([x(8,k),x(9,k)])])]);#CycA
+    max([min([1-x(1,k),1-x(4,k),1-x(5,k),1-x(10,k)]),min([x(6,k),1-min([x(4,k),x(5,k)]),1-x(10,k),1-x(1,k)])]);#p27
+    x(10,k);#Cdc20
+    max([min([1-x(5,k),1-x(10,k)]),x(7,k),min([x(6,k),1-x(10,k)])]);#Cdh1
+    max([1-x(8,k),min([x(8,k),x(9,k),max([x(7,k),x(5,k),x(10,k)])])]);#UbcH10
+    min([1-x(7,k),1-x(8,k)])#CycB
     ];
 endfunction
 
@@ -64,26 +54,16 @@ function y=f_patho(x,k)
     #to cope with both boolean and multivalued logic, the Zadeh fuzzy logic
     #operators are used
     y=[
-    #CycD
-    x(1,k);
-    #Rb
-    0;
-    #E2F
-    max([min([1-x(2,k),1-x(5,k),1-x(10,k)]),min([x(6,k),1-x(2,k),1-x(10,k)])]);
-    #CycE
-    min([x(3,k),1-x(2,k)]);
-    #CycA
-    max([min([x(3,k),1-x(2,k),1-x(7,k),1-min([x(8,k),x(9,k)])]),min([x(5,k),1-x(2,k),1-x(7,k),1-min([x(8,k),x(9,k)])])]);
-    #p27
-    max([min([1-x(1,k),1-x(4,k),1-x(5,k),1-x(10,k)]),min([x(6,k),1-min([x(4,k),x(5,k)]),1-x(10,k),1-x(1,k)])]);
-    #Cdc20
-    x(10,k);
-    #Cdh1
-    max([min([1-x(5,k),1-x(10,k)]),x(7,k),min([x(6,k),1-x(10,k)])]);
-    #UbcH10
-    max([1-x(8,k),min([x(8,k),x(9,k),max([x(7,k),x(5,k),x(10,k)])])]);
-    #CycB
-    min([1-x(7,k),1-x(8,k)])
+    x(1,k);#CycD
+    0;#Rb
+    max([min([1-x(2,k),1-x(5,k),1-x(10,k)]),min([x(6,k),1-x(2,k),1-x(10,k)])]);#E2F
+    min([x(3,k),1-x(2,k)]);#CycE
+    max([min([x(3,k),1-x(2,k),1-x(7,k),1-min([x(8,k),x(9,k)])]),min([x(5,k),1-x(2,k),1-x(7,k),1-min([x(8,k),x(9,k)])])]);#CycA
+    max([min([1-x(1,k),1-x(4,k),1-x(5,k),1-x(10,k)]),min([x(6,k),1-min([x(4,k),x(5,k)]),1-x(10,k),1-x(1,k)])]);#p27
+    x(10,k);#Cdc20
+    max([min([1-x(5,k),1-x(10,k)]),x(7,k),min([x(6,k),1-x(10,k)])]);#Cdh1
+    max([1-x(8,k),min([x(8,k),x(9,k),max([x(7,k),x(5,k),x(10,k)])])]);#UbcH10
+    min([1-x(7,k),1-x(8,k)])#CycB
     ];
 endfunction
 
